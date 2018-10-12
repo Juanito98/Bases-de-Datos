5a4e34236d7eca0bc82c436852267fae06fd40c1
LORENA PATRICIA BARRERA RODRIGUEZ <lbarrer8@itam.mx>
2018-10-12 16:11:43 -0500

    Registro Compras

---
+++ Cadenas/Cadenas/Tablas/RegistroCompra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadenas.Tablas
+{
+    public partial class RegistroCompra : Form
+    {
+        public RegistroCompra()
+        {
+            InitializeComponent();
+        }
+    }
+}
